231bf45de4f930203f6b11a10e1e3e4c49e17fcc
Trinh Duc Thien <td.thien@shopmacher.de>
2018-05-30 00:48:47 +0700

    fm-elfinder-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    FM\ElfinderBundle\FMElfinderBundle::class => ['all' => true],
 ];
